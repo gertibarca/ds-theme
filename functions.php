af5a6edd8581775a8349f775d480d53c77204df6
gertibarca <gertinovoberdaliu@gmail.com>
2026-03-02 19:11:05 +0100

    1

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
-<?php 
-// 1. Theme setup (Featured images, title tags, etc.)
+<?php
+// 1. Theme setup
 function dstheme_setup() { 
     add_theme_support( 'post-thumbnails' ); 
     add_theme_support( 'post-formats', array( 'aside', 'image', 'video' ) ); 
@@ -7,26 +7,107 @@ function dstheme_setup() {
 } 
 add_action( 'after_setup_theme', 'dstheme_setup' ); 
 
-// 2. Load all Styles and Scripts
+
+// 2. Load Styles and Scripts
 function dstheme_assets() { 
-    // Load Bootstrap CSS FIRST (Using the correct 5.3.3 version)
-    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all' ); 
 
-    // Load your style.css NEXT
-    wp_enqueue_style( 'ds-style', get_stylesheet_uri(), array('bootstrap-css'), '1.0', 'all' ); 
+    wp_enqueue_style( 'bootstrap-css',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+        array(),
+        '5.3.3',
+        'all'
+    ); 
 
-    // Load your slider.css
-    wp_enqueue_style( 'slider-style', get_template_directory_uri() . '/css/slider.css', array('bootstrap-css'), '1.0', 'all' ); 
+    wp_enqueue_style( 'ds-style',
+        get_stylesheet_uri(),
+        array('bootstrap-css'),
+        '1.0',
+        'all'
+    ); 
 
-    // Load Bootstrap JS
-    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), '5.3.3', true );
+    wp_enqueue_style( 'slider-style',
+        get_template_directory_uri() . '/css/slider.css',
+        array('bootstrap-css'),
+        '1.0',
+        'all'
+    ); 
 
-    // Load your custom.js
-    wp_enqueue_script( 'customjs', get_template_directory_uri() . '/js/custom.js', array( 'jquery', 'bootstrap-js' ), '1.0', true ); 
+    wp_enqueue_script( 'bootstrap-js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+        array(),
+        '5.3.3',
+        true
+    );
 
-    // Comment reply script
+    wp_enqueue_script( 'customjs',
+        get_template_directory_uri() . '/js/custom.js',
+        array( 'jquery', 'bootstrap-js' ),
+        '1.0',
+        true
+    ); 
+
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) { 
         wp_enqueue_script( 'comment-reply' ); 
     } 
 } 
 add_action( 'wp_enqueue_scripts', 'dstheme_assets' );
+
+
+// 3. Sidebar
+function themename_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => 'Primary Sidebar',
+        'id'            => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+
+}
+
+add_action( 'widgets_init', 'themename_widgets_init' );
+
+// Create Widget Class
+class Foo_Widget extends WP_Widget {
+
+    // Constructor
+    public function __construct() {
+        parent::__construct(
+            'foo_widget',                 // Widget ID
+            'A Foo Widget',               // Widget Name
+            array(
+                'description' => 'Simple Hello World widget'
+            )
+        );
+    }
+
+    // Frontend output
+    public function widget( $args, $instance ) {
+
+        echo $args['before_widget'];
+
+        echo '<p>Gerti</p>';
+
+        echo $args['after_widget'];
+    }
+
+    // Widget form (Admin area)
+    public function form( $instance ) {
+        echo '<p>No options yet.</p>';
+    }
+
+    // Save widget options
+    public function update( $new_instance, $old_instance ) {
+        return $new_instance;
+    }
+}
+
+
+// Register Widget
+function register_foo_widget() {
+    register_widget( 'Foo_Widget' );
+}
+
+add_action( 'widgets_init', 'register_foo_widget' );
--- index.php
+++ index.php
@@ -10,64 +10,65 @@
 
 <div id="main-content" class="container">
     <div class="row">
-        
+
+        <!-- MAIN CONTENT -->
         <div class="col-lg-8">
             <?php if ( have_posts() ) : ?>
                 <div class="row">
                     <?php while ( have_posts() ) : the_post(); ?>
-                        <div class="col-md-12 mb-4">
+                        <div class="col-12 mb-4">
                             <div class="card shadow-sm h-100">
+
                                 <?php if ( has_post_thumbnail() ) : ?>
-                                    <div class="post-thumbnail">
-                                        <?php the_post_thumbnail('medium_large', ['class' => 'card-img-top img-fluid']); ?>
-                                    </div>
+                                    <?php the_post_thumbnail(
+                                        'medium_large',
+                                        array( 'class' => 'card-img-top img-fluid' )
+                                    ); ?>
                                 <?php endif; ?>
-                                
+
                                 <div class="card-body">
                                     <h2 class="card-title h4">
                                         <a href="<?php the_permalink(); ?>" class="text-decoration-none text-dark">
                                             <?php the_title(); ?>
                                         </a>
                                     </h2>
+
                                     <p class="card-text text-muted small">
                                         Posted on <?php echo get_the_date(); ?> by <?php the_author(); ?>
                                     </p>
-                                    <div class="card-text">
+
+                                    <div class="card-text mb-3">
                                         <?php the_excerpt(); ?>
                                     </div>
-                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm">Read More</a>
+
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm">
+                                        Read More
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     <?php endwhile; ?>
                 </div>
 
+                <!-- Pagination -->
                 <div class="d-flex justify-content-center my-4">
                     <?php the_posts_pagination( array(
                         'mid_size'  => 2,
                         'prev_text' => '« Previous',
                         'next_text' => 'Next »',
-                        'class'     => 'pagination'
                     ) ); ?>
                 </div>
 
             <?php else : ?>
-                <div class="alert alert-info">No posts found.</div>
+                <div class="alert alert-info">
+                    No posts found.
+                </div>
             <?php endif; ?>
         </div>
 
+        <!-- SIDEBAR -->
         <div class="col-lg-4">
-            <div class="p-4 mb-3 bg-light rounded">
-                <h4 class="fst-italic">About the Blog</h4>
-                <p class="mb-0">This is a Bootstrap-powered WordPress theme. Every element here is responsive and styled using the 12-column grid system.</p>
-            </div>
-            
-            <div class="p-4">
-                <h4 class="fst-italic">Archives</h4>
-                <ul class="list-unstyled">
-                    <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-                </ul>
-            </div>
+            <?php get_sidebar( 'primary' ); ?>
         </div>
 
     </div>
